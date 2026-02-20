5f8cd7a18214aaf2d2a8863fbe1dc7bd978ff08d
Joaquín <32976960+JODOSA@users.noreply.github.com>
2026-02-20 08:51:42 +0100

    feat: add validation annotations tu User entity

--- src/main/java/com/recetas/recetasapi/entity/User.java
+++ src/main/java/com/recetas/recetasapi/entity/User.java
@@ -2,6 +2,10 @@ package com.recetas.recetasapi.entity;
 
 import com.recetas.recetasapi.enums.UserRole;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,26 +24,37 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "El username no puede estar vacío")
+    @Size(min = 3, max = 50, message = "El username debe tener entre 2 y 50 caracteres")
     @Column(nullable = false, unique = true, length = 50)
     private String username;
 
+    @NotBlank(message = "El email no puede estar vacío")
+    @Email(message = "Formato de email inválido")
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Size(min = 8, message = "La contraseña debe tener mínimo 8 caracteres")
     @Column(nullable = false)
     private String password;
 
-    @Column(length = 50)
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(max = 50, message = "El nombre no puede superar los 50 caracteres")
+    @Column(nullable = false, length = 50)
     private String firstName;
 
-    @Column(length = 50)
+    @NotBlank(message = "El apellido no puede estar vacío")
+    @Size(max = 50, message = "El apellido no puede superar los 50 caracteres")
+    @Column(nullable = false, length = 50)
     private String lastName;
 
+    @NotNull(message = "El rol no puede ser nulo")
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private UserRole role = UserRole.USER;
+    @Column(nullable = false, length = 20)
+    private UserRole role; // = UserRole.USER;
 
     @CreationTimestamp
-    @Column(updatable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 }
